381bbfec406929d3e3c1d80266d8f880f734b22c
lucasbueno-telus <lucas.bueno@telus.com>
2024-12-27 00:31:42 -0300

    Add title to login screen

--- app/src/main/java/com/lucasbueno/basiclogin/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/lucasbueno/basiclogin/presentation/login/LoginScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,6 +92,14 @@ fun ScreenContent(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
+            Text(
+                text = "Welcome to the Login App",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.padding(16.dp)
+            )
+
             OutlinedTextField(
                 value = email,
                 onValueChange = { email = it },
