58134b28cd39f796153f883a0cd431293d673bbd
lucasbueno-telus <lucas.bueno@telus.com>
2024-12-28 23:05:19 -0300

    Add ConstantsTemplate.kt

---
+++ app/src/main/java/com/lucasbueno/basiclogin/ConstantsTemplate.kt
@@ -0,0 +1,9 @@
+package com.lucasbueno.basiclogin
+
+object ConstantsTemplate {
+    /*
+     This is a template containing the same variables of Constants.kt.
+     Add here your own keys
+     */
+    const val WEB_CLIENT_ID = "ADD HERE YOUR WEB CLIENT ID"
+}
